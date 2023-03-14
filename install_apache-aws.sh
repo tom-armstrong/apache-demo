444e3eb6e55da741d31a246522ca608195b7a341
Tom Armstrong <56370513+tom-armstrong@users.noreply.github.com>
2023-03-14 11:47:41 -0700

    Add files via upload

---
+++ install_apache-aws.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cld=aws
+#cld=gcp
+sudo touch /var/lib/man-db/auto-update  # slow mand-db update bug
+sudo apt update -y
+sudo apt install apache2 -y
+sudo systemctl start apache2
+sudo mv index-${cld}.html /var/www/html/index.html
+sudo mv ${cld}-graphic.gif /var/www/html/
